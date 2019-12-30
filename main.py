8af05575f596daed0cfaf5f7123571e6910373ee
TheMrPhantom <diemilbe@gmx.net>
2019-12-30 01:12:01 +0100

    download probably working

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import PythonTelegramWraper.bot as bot
 from bs4 import BeautifulSoup
-import requests
-import dryscrape
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 def start(update, context):
     context.bot.sendMessage(bot.chatID(update),"Register your Beatsaver page with /register <url>")
@@ -14,22 +15,20 @@ def register(update, context):
 def check(update, context):
     uID=bot.chatID(update)
     url=bot.user(uID)
-    print("response")
     html=None
 
-    html=requests.get('https://beatsaver.com/uploader/5e08e3dd30cd920006c143dd').text
-    print("response")
-    session = dryscrape.Session()
-    print("response")
-    sess.set_header('user-agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0')
-    sess.set_attribute('auto_load_images', False)          
-    sess.set_timeout(360)
-    print("response")
-    session.visit("https://beatsaver.com/uploader/5e08e3dd30cd920006c143dd")
-    print("response")
-    response = session.body()
+    options = Options()
+    options.headless = True
 
-    print(response)
+    print("a")
+    driver = webdriver.Firefox(options=options)
+    print("b")
+    driver.get("http://www.tarlabs.com")
+    print("c")
+    
+    html=driver.page_source
+
+    print(html)
     html=BeautifulSoup(html)
     print(html)
 
--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
- 
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.headless = True
+
 # you can use driver = webdriver.PhantomJS() 
 # if you want a headless browser. 
 # You need to have PhantomJS installed in path
 print("a")
-driver = webdriver.Firefox()
+driver = webdriver.Firefox(options=options)
 print("b")
 driver.get("http://www.tarlabs.com")
 print("c")
